97994f1f2e8e31b9efa1012037c8b2175e33ab23
“Santhosh” <santhoshvelr@gmail.com>
2020-06-23 19:22:13 +0530

    card component created

--- client/src/components/home/Cards.js
+++ client/src/components/home/Cards.js
@@ -1,104 +1,69 @@
 import React from 'react'
-import {Card, CardBody} from 'reactstrap';
+import { Card, CardBody } from 'reactstrap';
 import './Cards.css'
 import Row from 'reactstrap/lib/Row';
 import Col from 'reactstrap/lib/Col';
 
+function Layout(props) {
+
+	return <Col lg="4" md="6" sm="4">
+		<Card>
+			<CardBody>
+				<h4>{props.title}</h4>
+				<h2 style={{ textAlign: "center" }}>
+					{props.count}
+				</h2>
+			</CardBody>
+		</Card>
+	</Col>
+
+}
+
+const today = [
+	{
+		title: "Sale",
+		count: 10
+	},
+	{
+		title: "Product",
+		count: 20
+	},
+	{
+		title: "Customer",
+		count: 15
+	}
+]
+
+const total = [
+	{
+		title: "Sale",
+		count: 40
+	},
+	{
+		title: "Product",
+		count: 50
+	},
+	{
+		title: "Customer",
+		count: 30
+	}
+]
+
 function Cards() {
-    return (
-		
-        <div className="cards">
+	return (
+
+		<div className="cards">
+			<h3>Today</h3>
 			<Row>
-				<Col lg="4" md="6" sm="4">
-					<Card>
-						<CardBody>
-						<h2>Design</h2>
-						<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-						minima iusto obcaecati culpa pariatur soluta exercitationem
-						nemo dolorum maiores incidunt eaque natus dolore tempora,
-						earum doloremque voluptatibus aliquam alias consequatur.
-						</p>
-						<a href="/">Read More</a>
-						</CardBody>
-					</Card>
-				</Col>
-				<Col lg="4" md="6" sm="4">
-					<Card>
-						<CardBody>
-						<h2>Design</h2>
-						<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-						minima iusto obcaecati culpa pariatur soluta exercitationem
-						nemo dolorum maiores incidunt eaque natus dolore tempora,
-						earum doloremque voluptatibus aliquam alias consequatur.
-						</p>
-						<a href="/">Read More</a>
-						</CardBody>
-					</Card>
-				</Col>
-				<Col className="col-middle" lg="4" md="6" sm="4">
-					<Card>
-						<CardBody>
-						<h2>Design</h2>
-						<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-						minima iusto obcaecati culpa pariatur soluta exercitationem
-						nemo dolorum maiores incidunt eaque natus dolore tempora,
-						earum doloremque voluptatibus aliquam alias consequatur.
-						</p>
-						<a href="/">Read More</a>
-						</CardBody>
-					</Card>
-				</Col>
+				{today.map((val, ind) => <Layout key={ind} title={val.title} count={val.count} />)}
 			</Row>
+			<h3>Total</h3>
 			<Row>
-				<Col lg="4" md="6" sm="4">
-					<Card>
-						<CardBody>
-						<h2>Design</h2>
-						<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-						minima iusto obcaecati culpa pariatur soluta exercitationem
-						nemo dolorum maiores incidunt eaque natus dolore tempora,
-						earum doloremque voluptatibus aliquam alias consequatur.
-						</p>
-						<a href="/">Read More</a>
-						</CardBody>
-					</Card>
-				</Col>
-				<Col lg="4" md="6" sm="4">
-					<Card>
-						<CardBody>
-						<h2>Design</h2>
-						<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-						minima iusto obcaecati culpa pariatur soluta exercitationem
-						nemo dolorum maiores incidunt eaque natus dolore tempora,
-						earum doloremque voluptatibus aliquam alias consequatur.
-						</p>
-						<a href="/">Read More</a>
-						</CardBody>
-					</Card>
-				</Col>
-				<Col className="col-middle" lg="4" md="6" sm="4">
-					<Card>
-						<CardBody>
-						<h2>Design</h2>
-						<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-						minima iusto obcaecati culpa pariatur soluta exercitationem
-						nemo dolorum maiores incidunt eaque natus dolore tempora,
-						earum doloremque voluptatibus aliquam alias consequatur.
-						</p>
-						<a href="/">Read More</a>
-						</CardBody>
-					</Card>
-				</Col>
+				{total.map((val, ind) => <Layout key={ind} title={val.title} count={val.count} />)}
 			</Row>
 		</div>
-		
-    )
+
+	)
 }
 
 export default Cards
