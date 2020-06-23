909866a82b9b379ecddb683a71c6f931c4a84465
“Santhosh” <santhoshvelr@gmail.com>
2020-06-23 20:52:08 +0530

    auth added sucessfully

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import NewForm from './forms/NewForm';
 import Home from "./home/Home";
 import Product from "./main/Product";
@@ -12,23 +12,105 @@ import TopNavBar from "./home/home-components/TopNavbar";
 
 import ProductView from "./main/ProductView";
 
-function App() {
-	return (
-		<div className="App">
-			<Router>
-				<TopNavBar />
-				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/form" exact component={NewForm} />
-					<Route path="/product" exact component={Product} />
-					<Route path="/product/:itemId" exact component={ProductView} />
-					<Route path="/order/:id" exact component={Order} />
-					<Route path="/user" exact component={User} />
-				</Switch>
-			</Router>
+import AuthApi from "./auth/AuthApi";
 
-		</div>
+import axios from "axios";
+
+import Login from "./login/Login";
+
+import FlexContainer from "./containers/FlexContainer";
+import RingLoader from "react-spinners/RingLoader";
+
+
+function Loading() {
+	return (
+		<FlexContainer>
+			<img
+				src="./images/logo.png"
+				alt=""
+				style={{
+					width: "50px",
+					height: "50px",
+					position: "relative",
+					top: "80px",
+					left: "7px"
+				}}
+			/>
+			<RingLoader size={100} color={"#C71585"} loading={true} />
+		</FlexContainer>
 	);
 }
+
+
+
+function App() {
+	const [auth, setAuth] = useState(false);
+	const [load, setLoad] = useState(true);
+
+	useEffect(() => {
+		axios.get("/auth")
+			.then(function (res) {
+				if (res.data.auth) {
+					setAuth(res.data);
+				}
+				setLoad(false);
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+	}, []);
+
+
+
+	return (
+		<Fragment>
+			{load ? <Loading /> : <div className="App">
+				<AuthApi.Provider value={{ auth, setAuth }}>
+					<Router>
+						<TopNavBar />
+						<Switch>
+							<RouteRegistration path="/sign" exact component={Login} />
+							<RouteProtected path="/" exact component={Home} />
+							<RouteProtected path="/form" exact component={NewForm} />
+							<RouteProtected path="/product" exact component={Product} />
+							<RouteProtected path="/product/:itemId" exact component={ProductView} />
+							<RouteProtected path="/order/:id" exact component={Order} />
+							<RouteProtected path="/user" exact component={User} />
+						</Switch>
+					</Router>
+				</AuthApi.Provider>
+			</div>
+			}
+		</Fragment>
+	);
+}
+
+
+const RouteRegistration = ({ component: Component, ...rest }) => {
+	const AthApi = React.useContext(AuthApi);
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				!AthApi.auth.auth ? <Component {...props} /> : <Redirect to="/" />
+			}
+		/>
+	);
+}
+
+
+
+const RouteProtected = ({ component: Component, ...rest }) => {
+	const AthApi = React.useContext(AuthApi);
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				AthApi.auth.auth ? <Component {...props} /> : <Redirect to="/sign" />
+			}
+		/>
+	);
+}
+
 
 export default App;
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,23 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { Button } from "reactstrap";
+import { Spinner } from 'reactstrap';
+
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+
+import AuthApi from "../auth/AuthApi";
+
+import axios from "axios";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 
 function Login() {
+  const authApi = React.useContext(AuthApi);
+  const [load, setLoad] = useState(false);
+  const [data, setData] = useState(
+    {
+      name: "",
+      password: ""
+    }
+  );
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState({
+    type: "",
+    content: ""
+  });
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setData(old => {
+      return {
+        ...old,
+        [name]: value
+      }
+    }
+    )
+  }
+
+  function handleSubmit() {
+    setLoad(true);
+    let prms = new URLSearchParams(data);
+    axios.post("/auth/signin", prms)
+      .then(function (res) {
+        if (res.data.auth) {
+          authApi.setAuth(res.data);
+          return;
+        }
+        else {
+          setMsg({
+            type: "error",
+            content: res.data.message
+          });
+          setOpen(true);
+        }
+        setLoad(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
   return (
-    <div className="back">
-      <div className="login-box">
-        <h1>Login</h1>
-        <div className="textbox">
-          <i className="fa fa-user" aria-hidden="true" />
-          <input type="text" placeholder="Username" />
+    <>
+      <div className="back">
+        <div className="login-box">
+          <h1>Login</h1>
+          <div className="textbox">
+            <i className="fa fa-user" aria-hidden="true" />
+            <input type="text" name="name" onChange={handleChange} placeholder="Username" />
+          </div>
+          <div className="textbox">
+            <i className="fa fa-lock" aria-hidden="true" />
+            <input type="password" name="password" onChange={handleChange} placeholder="Password" />
+          </div>
+          <Button className="button" onClick={handleSubmit} >Sign In {load && <Spinner size="sm" color="light" />}</Button>
         </div>
-        <div className="textbox">
-          <i className="fa fa-lock" aria-hidden="true" />
-          <input type="password" placeholder="Password" />
-        </div>
-        <Button className="button">Sign In</Button>
       </div>
-    </div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={msg.type}>
+          {msg.content}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
